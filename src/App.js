386fdb86424ee5faa11d5768bf7341833f5459b7
Lee Seo Yeon <leesy020411@naver.com>
2023-05-22 14:25:03 +0900

    3. Props,state 사용하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import Hello from './Hello';
+import Todo from './Todo';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props){  //매개변수 props 생성자
+    super(props);  //매개변수 pros 초기화
+    this.state ={  //item에 item.id, item.title, item.done 매개변수 이름과 값 할당
+      item :{id:0, title:"Hello World 1 ", done:true},
+    };
+  }
   render(){
-    return (
+    return (  //매개변수 item에 변수명/값을 전달
       <div className="App">
-        <Hello />
+        <Todo item ={this.state.item}/>
       </div>
     );
   }
